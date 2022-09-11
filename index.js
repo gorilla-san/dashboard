b60e14ed762821773770d4bd9e940666870d01da
gorilla-san <108207000+gorilla-san@users.noreply.github.com>
2022-09-11 20:46:04 +0200

    Add files via upload
    Layout and some responsiveness

---
+++ index.js
@@ -0,0 +1,21 @@
+    const collapse = document.querySelector(".sidebar")
+    const arrow = document.querySelector(".sidebar-collapse")
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 750) {
+        collapse.classList.remove("visible"); arrow.classList.remove("open")
+    }
+  });
+
+function collapseSidebar () {
+    if ( collapse.classList.contains("visible")) {
+        collapse.classList.remove("visible");
+    }
+    else collapse.classList.add("visible");
+
+    if (arrow.classList.contains("open")) {
+        arrow.classList.remove("open");
+    }
+    else arrow.classList.add("open");
+
+}
